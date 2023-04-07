19be0b60bc76aa418ded2b30152e48d604b538f0
Baktybeks <baktybeks@gmail.com>
2023-04-08 00:10:49 +0600

    feat: add favicon, slices (08.04.2023 00:10 am)

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
+import errorReducer from "./slices/errorSlice"
+import preloaderReducer from "./slices/preloaderSlice"
+
 
 export const store = configureStore({
     reducer: {
-
+        errorReducer,
+        preloaderReducer,
     }
 })
